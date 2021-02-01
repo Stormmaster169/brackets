4de51458bc553b1fdc3d177ad3d20bf1c8430661
Stormmaster169 <Stormmaster169@gmail.com>
2021-02-01 22:25:08 +0300

    init: start Brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let counters = new Object();
+  bracketsConfig.map(el => el[0] === el[1] ? counters[el[0]] = 0 : el);
+  let newBracketsConfig = bracketsConfig.map(el => el[0] === el[1] ? el.map((e, i) => i === 1 ? e.repeat(2) : e) : el);
+  let open = new Set(newBracketsConfig.map(newBracketsConfig => newBracketsConfig[0]));
+  let close = new Set(newBracketsConfig.map(newBracketsConfig => newBracketsConfig[1]));
+  let relevant = newBracketsConfig.reduce((acc, [open, close]) => ({ ...acc, [close]: open }), {});
+  let stack = [];
+  for (let char of str) {
+    if (char in counters && counters[char] === 0) {
+      counters[char] = 1;
+    } else if (char in counters && counters[char] === 1) {
+      counters[char] = 0;
+      char = char.repeat(2);
+    }
+    if (open.has(char)) {
+      stack.push(char);
+    }
+    if (close.has(char)) {
+      if (relevant[char] !== stack.pop()) return false;
+    }
+  }
+
+  return stack.length === 0;
+};
